3f642eaf1d5faef9b128fd2ef117ad7cd8e13fbb
Atharva <113771966+Atharv-3105@users.noreply.github.com>
2026-07-12 12:34:41 +0530

    Add: sort-colors - submission-0

---
+++ Data Structures & Algorithms/sort-colors/submission-0.go
@@ -0,0 +1,22 @@
+func sortColors(nums []int) {
+    low := 0
+	mid := 0
+	high := len(nums) - 1
+
+	for mid <= high {
+		if nums[mid] == 0 {
+			temp := nums[low]
+			nums[low] = nums[mid]
+			nums[mid] = temp
+			low++
+			mid++
+		}else if nums[mid] == 1 {
+			mid++
+		}else{
+			temp := nums[high]
+			nums[high] = nums[mid]
+			nums[mid] = temp
+			high--
+		}
+	}
+}
